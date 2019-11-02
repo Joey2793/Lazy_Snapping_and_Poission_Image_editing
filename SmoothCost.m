c7d6f3780cab6473dce9cded86998626d0552d6d
Joey2793 <43018104+Joey2793@users.noreply.github.com>
2019-11-02 16:38:27 -0400

    Update SmoothCost.m

--- SmoothCost.m
+++ SmoothCost.m
@@ -1,28 +1,8 @@
 function pairwise1 = SmoothCost(rgbvector,H,W,sigma )
 if nargin < 4, sigma = 1; end
 pairwise1 = sparse(H*W,H*W);
-% for row = 0:H-1
-%   for col = 0:W-1
-%     pixel = 1+ row*W + col;
-%     %from up to down
-%     if row+1 < H 
-%         w_dif =exp(-1/2*((norm(rgbvector(pixel,:)-rgbvector(1+col+(row+1)*H,:))/sigma)^2));
-%         pairwise1(pixel, 1+col+(row+1)*H) = w_dif; 
-%     end
-%     %from down to up
-%     if row-1 >= 0
-%         w_dif = exp(-1/2*((norm(rgbvector(pixel,:)-rgbvector(1+col+(row-1)*H,:))/sigma)^2));
-%         pairwise1(pixel, 1+col+(row-1)*H) = w_dif; end 
-%     % to down
-%     if col+1 < W
-%         w_dif = exp(-1/2*((norm(rgbvector(pixel,:)-rgbvector(1+(col+1)+row*W,:))/sigma)^2));
-%         pairwise1(pixel, 1+(col+1)+row*W) = w_dif; end
-%     %to up
-%     if col-1 >= 0
-%         w_dif = exp(-1/2*((norm(rgbvector(pixel,:)-rgbvector(1+(col-1)+row*W,:))/sigma)^2));
-%         pairwise1(pixel, 1+(col-1)+row*W) = w_dif; end 
-%   end
-% end
+
+
 for N = 1:H*W
     reminder = rem(N,H);
     result = fix(N/H);
